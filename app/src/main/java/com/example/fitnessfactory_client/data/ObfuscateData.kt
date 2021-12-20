20c1527c204fe6bbe0018402f39424d73f4641c9
flapy.list@gmail.com <Lollipopjesus228>
2021-12-20 19:16:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fitnessfactory_client/data/ObfuscateData.kt
@@ -0,0 +1,9 @@
+package com.example.fitnessfactory_client.data
+
+import io.michaelrocks.paranoid.Obfuscate
+
+@Obfuscate
+object ObfuscateData {
+
+    fun getWebClientId() : String = "660172613611-o6u1a4c07k1hn4re7loe9k3jvoe6qr99.apps.googleusercontent.com"
+}
---
+++ app/src/main/java/com/example/fitnessfactory_client/data/system/FirebaseAuthManager.kt
@@ -0,0 +1,6 @@
+package com.example.fitnessfactory_client.data.system
+
+class FirebaseAuthManager {
+
+
+}
