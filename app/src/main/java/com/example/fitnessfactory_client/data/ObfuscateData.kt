5f2829dfb116119eb9b6f0a0c47bb795a31d8e87
flapy.list@gmail.com <Lollipopjesus228>
2021-12-28 17:03:58 +0300

    authorization screen ready/ authorization works. Database work is to be done

--- app/src/main/java/com/example/fitnessfactory_client/data/ObfuscateData.kt
+++ app/src/main/java/com/example/fitnessfactory_client/data/ObfuscateData.kt
@@ -5,5 +5,5 @@ import io.michaelrocks.paranoid.Obfuscate
 @Obfuscate
 object ObfuscateData {
 
-    fun getWebClientId() : String = "660172613611-o6u1a4c07k1hn4re7loe9k3jvoe6qr99.apps.googleusercontent.com"
+    fun getWebClientId() : String = "660172613611-8pfj2g0o7h8snhtuc0ipoc7i37if9odp.apps.googleusercontent.com"
 }
